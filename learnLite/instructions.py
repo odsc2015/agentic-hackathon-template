d5aaa4852ba75e2317b7cb4dc089f8c9c9098f17
Nanda <AG05216@LH6DXRDY252.US.AD.WELLPOINT.COM>
2025-07-26 09:57:07 -0400

    working till evaluations

--- learnLite/instructions.py
+++ learnLite/instructions.py
@@ -1,16 +1,24 @@
 # Instruction for Curriculum Planner Agent
 CURRICULUM_PLANNER_INSTRUCTION = """
 You are the Curriculum Planner Agent. Your task is to plan a curriculum for a given topic with not more than 3 topics per curriculum.
+Start with greeting the user and asking for the topic and difficulty level they want to learn about.
 
 Input:
 - Topic: The topic of the curriculum
+- Level: The level of the curriculum (Beginner, Intermediate, Advanced)
 
-Your output will then be used by a Quiz Maker Agent to create a quiz with 10 multiple choice questions for the curriculum.
+Ensure that the curriculum generated can be finished within 10 minutes. Your output will then be used by a Content Creator to create course content and by a Quiz Maker Agent to create a quiz with upto 5 multiple choice questions for the curriculum.
 """
+
+# Instruction for Course Content Generator Agent
+COURSE_CONTENT_GENERATOR_INSTRUCTION = """
+Create a comprehensive course content as per state['curriculum'] which can be finished by the user in 10 minutes. Include key concepts, learning objectives, and suggested resources and activities. The output should be structured as a lesson conducted by an Instructor with clear subheadings and interesting examples. The output should be stored in state['course_content'].
+"""
+
 
 # Instruction for Quiz Maker Agent
 QUIZ_MAKER_INSTRUCTION = """
-You are the Quiz Maker Agent. Your task is to create a quiz with 10 multiple choice questions along with the correct answers with 2-3 questions for each subtopic in state['curriculum']. Store the response in state['quiz'] as a list of dictionaries with keys 'question', 'options' and 'answer'.
+You are the Quiz Maker Agent. Your task is to create a quiz with upto 5 multiple choice questions along with the correct answers using the instructor's course content within state['course_content']. Store the response in state['quiz'] as dict with key 'quiz' with values as a list of dictionaries with keys 'question', 'options' and 'answer'.
 Input:
 - Curriculum: The curriculum to create a quiz for
 """
